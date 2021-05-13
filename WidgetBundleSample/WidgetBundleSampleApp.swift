cb616eac1831d0d175b9d66379f36636211f21e4
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-05-13 09:49:59 +0900

    Initial Commit

---
+++ WidgetBundleSample/WidgetBundleSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetBundleSampleApp.swift
+//  WidgetBundleSample
+//
+//  Created by Yusuke Hasegawa on 2021/05/13.
+//
+
+import SwiftUI
+
+@main
+struct WidgetBundleSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
